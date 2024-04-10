2e942147ae2fe7efe215a21c9fd3860b482142a1
Atharva Patil <patil8atharva@gmail.com>
2024-04-10 20:50:22 +0530

    added basic tests for all types

--- bencoding/bdecode_test.go
+++ bencoding/bdecode_test.go
@@ -5,11 +5,41 @@ import (
 	"testing"
 )
 
-//TODO: implement all tests
+//TODO: implement more tests (nested lists, dicts, etc.)
 
 func TestInteger(t *testing.T) {
 	source := "i34e"
 	expected := bencoding.BValue{bencoding.BTypes.Integer, 34}
+	value, _ := bencoding.Decode(source)
+
+	if value != expected {
+		t.Errorf("Expected %v got %v", expected, value)
+	}
+}
+
+func TestString(t *testing.T) {
+	source := "11:Hello World"
+	expected := bencoding.BValue{bencoding.BTypes.String, "Hello World"}
+	value, _ := bencoding.Decode(source)
+
+	if value != expected {
+		t.Errorf("Expected %v got %v", expected, value)
+	}
+}
+
+func TestList(t *testing.T) {
+	source := "l4:spam4:eggse"
+	expected := bencoding.BValue{bencoding.BTypes.List, []bencoding.BValue{{bencoding.BTypes.String, "spam"}, {bencoding.BTypes.String, "eggs"}}}
+	value, _ := bencoding.Decode(source)
+
+	if value != expected {
+		t.Errorf("Expected %v got %v", expected, value)
+	}
+}
+
+func TestDict(t *testing.T) {
+	source := "d3:cow3:moo4:spam4:eggse"
+	expected := bencoding.BValue{bencoding.BTypes.Dict, map[string]bencoding.BValue{"cow": {bencoding.BTypes.String, "moo"}, "spam": {bencoding.BTypes.String, "eggs"}}}
 	value, _ := bencoding.Decode(source)
 
 	if value != expected {
